02365b24a3ed5a604e9d4df10bb441e39284797f
RishDevil <rishabhchaubey13@gmail.com>
2020-12-24 18:18:27 +0530

    saving

--- task/src/component/Login.js
+++ task/src/component/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
 import queryString from "query-string";
-
+import { CLIENT_ID, SECRET_KEY } from "../key";
 import Cookies from "js-cookie";
 import Style from "./Login.module.css";
 import { LoginIcon, GitIcon } from "../icon/Icon";
@@ -17,8 +17,8 @@ const Login = (props) => {
       Axios.post(
         "/auth",
         {
-          client_id: "893aad47e9596025d42c",
-          client_secret: "16ac8e8a8c7722bc22d9a3580abfccc1ec257074",
+          client_id: CLIENT_ID,
+          client_secret: SECRET_KEY,
           code,
         },
         {
@@ -43,7 +43,13 @@ const Login = (props) => {
       <div className={Style.circle3}></div>
       <div className={Style.circle4}></div>
       <div className={Style.sub_con}>
-        <a href="https://github.com/login/oauth/authorize?client_id=893aad47e9596025d42c&client_secret=16ac8e8a8c7722bc22d9a3580abfccc1ec257074&scope=repo,user,email">
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=
+            ${CLIENT_ID} 
+            &client_secret=
+            ${SECRET_KEY} 
+            &scope=repo,user,email`}
+        >
           <div className={Style.link}>
             <img src={GitIcon} className={Style.icon} alt="" /> login{" "}
             <img src={LoginIcon} alt="" />
